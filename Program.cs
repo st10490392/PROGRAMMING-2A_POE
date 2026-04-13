cf47ad3d767c16aa3d99bc137f97eb2dd90e8211
st10490392 <st10490392@rcconnect.edu.za>
2026-04-13 20:21:21 +0200

    main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Chatbot bot = new Chatbot();
+        bot.start();
+    }
+} 
